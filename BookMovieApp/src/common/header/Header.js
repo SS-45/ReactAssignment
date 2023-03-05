53b0761dc90398854b6bbb1463ac3eab8b0c05a7
SS-45 <srikantsahu45@gmail.com>
2023-03-05 22:06:17 +0530

    details basic ui

--- BookMovieApp/src/common/header/Header.js
+++ BookMovieApp/src/common/header/Header.js
@@ -9,7 +9,9 @@ export default function Header() {
   return (
     <header id="header">
         <img id="logo" src={logo} />
+
         <Button  variant="contained" style={{ float: 'right'}}>Login</Button>
+         <Button  id="btnbookshow" variant="contained" style={{ float: 'right'}}>Book Show</Button>
     </header>
   );
 }
--- BookMovieApp/src/screens/home/Home.js
+++ BookMovieApp/src/screens/home/Home.js
@@ -1,11 +1,16 @@
 import React from 'react';
+
 import Header from "../../common/header/Header";
 import "./Home.css";
 
 function Home() {
+
+
   return(
    <div><Header/>
-   <h1 id="heading">Upcoming Movies</h1></div>
+   <h1 id="heading">Upcoming Movies</h1>
+
+   </div>
   );
 }
 
