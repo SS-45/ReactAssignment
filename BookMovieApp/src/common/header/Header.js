79fe7db5c7ff3c25b3b16fdd9665c05ec63234c0
SS-45 <srikantsahu45@gmail.com>
2023-03-05 20:08:40 +0530

    Header basic ui

---
+++ BookMovieApp/src/common/header/Header.js
@@ -0,0 +1,15 @@
+import { AppBar, Toolbar, Button } from "@material-ui/core";
+import React from "react";
+import "./Header.css";
+import logo from "../../assets/logo.svg";
+
+export default function Header() {
+
+
+  return (
+    <header id="header">
+        <img id="logo" src={logo} />
+        <Button  variant="contained" style={{ float: 'right'}}>Login</Button>
+    </header>
+  );
+}
